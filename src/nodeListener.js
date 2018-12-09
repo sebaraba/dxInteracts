f4561ad228eac433a7862a66e0b249f8bf654a02
sebaraba <rabasebastians@gmail.com>
2018-12-09 15:54:58 +0000

    add node listener

--- src/nodeListener.js
+++ src/nodeListener.js
@@ -1,6 +1,21 @@
 //import contracts
 
-const myObservable = new Observable((observer) => {
+// Creating and sending the new request to the smart contract
+const newRequest = (options) => {
+    reutrn Promise.resolve({
+
+    });
+}
+
+// If the user wants to trade eth it gets wrapped
+const wrapEth = () => {
+    reutrn Promise.resolve({
+    
+    });
+}
+
+// Fire an observable that searches for the appropiate auction
+const searchAuction = new Observable((observer) => {
     const datasource = new DataSource();
     datasource.ondata = (e) => observer.next(e);
     datasource.onerror = (err) => observer.error(err);
@@ -9,3 +24,24 @@ const myObservable = new Observable((observer) => {
         datasource.destroy();
     };
 });
+
+// If the user wants to trade eth it gets wrapped
+const depositDesiredToken = () => {
+    reutrn Promise.resolve({
+
+    });
+}
+
+const endOfAuction = new Observable((observer) => {
+    const datasource = new DataSource();
+    datasource.ondata = (e) => observer.next(e);
+    datasource.onerror = (err) => observer.error(err);
+    datasource.oncomplete = () => observer.complete();
+    return () => {
+        datasource.destroy();
+    };
+});
+
+const sendPushNotificationCompleteTx = (amount, address) => {
+    
+}
